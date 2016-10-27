cdbf1b4b75375960731bfa35c6f9dc01e292c042
Subramanian <SXS105@lennoxintl.com>
2016-10-27 16:41:02 +0530

    initial commit

---
+++ ClientApp/BackendService/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace BackendService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string deviceID = (args.Count() > 0) ? args[0] : "device1";
+            
+
+            var message = new DeviceMessage()
+            {
+                DeviceID = deviceID
+            };
+
+          //  StringContent content = new StringContent(JsonConvert.SerializeObject(message));
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8257/");
+            client.DefaultRequestHeaders.Accept.Clear();
+          
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List all Names.  
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8,"application/json"); 
+
+            HttpResponseMessage response = client.PostAsync("api/Values", content).Result;
+
+        }
+
+        public class DeviceMessage
+        {
+           public string DeviceID { get; set; }
+        }
+
+    }
+}
---
+++ ServiceApp/SignalRStatelessService/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using SignalRStatelessService.SignalR;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SignalRStatelessService.Controllers
+{
+    [ServiceRequestActionFilter]
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public void Post([FromBody] DeviceMessage message)
+        {
+            var deviceID = message.DeviceID;
+            Notifier notify = new Notifier();
+            notify.Notify(deviceID, "from server message" );
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
